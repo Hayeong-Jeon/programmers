3164479c7eee18da606ca592de0a4db3d0015fcd
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-10 22:37:47 +0900

    Update 최댓값과 최솟값.py

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,12 +1,13 @@
 def solution(s):
-    answer= []
-    num = [int(i) for i in s.split(' ')]
+    answer=[]
+    num = [int(i) for i in s.split(' ')]   #공백을 기준으로 나누어 num에 저장
 
-    answer.append(str(min(num)))
+    answer.append(str(min(num)))   #answer에 num배열 중 가장 큰 값, 작은 값을 문자열로 저장
     answer.append(str(max(num)))
-
+    
     string =''
     for i in answer:
-        string += i + ' '
-    
+        string += i + ' '    #string에는 최종적으로 "작은값 큰값 "으로 저장 되기때문에
+        					 #마지막에 저장된 공백을 제거하기 위해 .strip()함수를 사용
+
     return string.strip()
