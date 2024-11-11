8ff4e5b81a4507118c104fbe6e0d7b02b04a8f09
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-11 17:45:02 +0900

    Update 최솟값 만들기.py

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -2,15 +2,12 @@ def solution(A,B):
     answer = 0
     lens = len(A)
 
-    A.sort(reverse=True)
-    B.sort()
+    A.sort(reverse=True)  		#내림차순 정렬
+    B.sort()			  		#오름차순 정렬
     
     sumAB=0
     
-    for i in range(lens):
+    for i in range(lens):		
         sumAB+=A[i] * B[i]
-    
-    print(A)
-    print(B)
 
     return sumAB
