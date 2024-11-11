ff55c854a671bde180281a2c82536008915cd771
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-11 21:57:44 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,21 +1,16 @@
-def solution(s):  # "  for the what  1what  "
+def solution(s):
     answer = ''
     lens = len(s)
-    print(lens)
-    string = [i for i in s.split(" ")]
-    s = s.title()
-    s_list = list(s)
+    string = s.title()						#문자열의 첫 글자마다 대문자로 변환
+    string = list(string)					#문자열을 리스트로 변환
         
     for i in range(lens):
-        if s[i] == " ":
+        if string[i] == " ":				#문자열에 공백이 있다면 넘어감
             continue
-        elif s[i].isdigit:
-            if i < lens -1:
-                s_list[i+1]=s_list[i+1].lower()
-            
-    print(s_list)
-    s=''.join(s_list)
-    print(s_list)
-    print(s)
+        elif string[i].isdigit:				#해당 문자가 숫자면
+            if i < lens -1:					#길이 체크해주고 숫자 뒤 문자를 소문자로변환
+                string[i+1] = string[i+1].lower()
+
+    answer = ''.join(string)				#리스트를 문자열로 변환
     
-    return s
+    return answer
