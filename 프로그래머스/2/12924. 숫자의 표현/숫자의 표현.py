7c88d0b11882c1d6c600e97132d5bc7375d9fdbf
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-13 22:33:31 +0900

    Update 숫자의 표현.py

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,15 @@
 def solution(n):
-    answer = 0
-    r=[]
-    c=0
-    for i in range(1,n+1):
-        s=0
-        j=i
-        for k in range(j,n+1):
-            s+=k
-            r.append(s)
-            if s==n :
-                c+=1
-            elif s>n: break
-    return c
+    count = 0
+    
+    for i in range(1,n+1):			# 1 ~ n까지 증가하도록
+        sumn = 0
+        j = i
+        for k in range(j,n+1):		# k는 1 ~ n, 2 ~ n ... n (연속된 숫자)
+            sumn += k				# 연속된 숫자들을 더하면서 n이 되는지 판단
+            
+            if sumn == n :			# n이 되는 연속된 숫자의 합이 있다면 count++
+                count += 1
+            elif sumn >= n:			# 합이 n을 넘어가면 중지
+                break
+    
+    return count
