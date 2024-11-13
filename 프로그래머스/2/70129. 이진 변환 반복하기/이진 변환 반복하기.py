842f4024f433cab3fc78fe6aa6ac3a1d4500e29f
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-13 22:33:57 +0900

    Update 이진 변환 반복하기.py

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,26 +1,15 @@
 def solution(s):
     string = s
-    sumn=0
-    count=0
+    sumn = 0									# 없앤 0의 갯수
+    count = 0									# 몇 번 반복했는지
 
-
-    while string != '1':
+    while string != '1':						# string이 '1'이 될 때 까지 반복
         
-        sumn+=string.count('0')
+        sumn += string.count('0')				# string에 있는 '0'의 갯수 카운트
         
-        string=bin(string.count('1'))
-        string = string[2:]
+        string = bin(string.count('1'))			# '1'의 갯수만큼 센 갯수를 이진수로 변환
+        string = string[2:]						# 변환 시'0bxx'로 변환되기에 '0b'없애주기
         
-        count+=1
+        count += 1								# 총 반복된 횟수
 
     return [count, sumn]
-
-
-#def solution(s):
-#    cnt, zero = 0, 0
-#    while s != '1' :
-#        zero += s.count('0')
-#        s = bin(s.count('1'))[2:]
-#        cnt+=1
-
-#    return [cnt, zero]
