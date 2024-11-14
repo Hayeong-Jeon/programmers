a063cafe633478c2185a2f04f973841f1079ee90
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-15 00:15:33 +0900

    Update 피보나치 수.py

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,19 +1,20 @@
 def solution(n):
     answer = True
-    count=2
-    bef = 0
-    aft = 1
-    sumn=bef+aft
-    result=0
+    bef = 0							# n - 2 값
+    aft = 1							# n - 1 값
+    sumn = bef + aft
+    count = 2						# sumn은 2번 째 값이기 때문에
+    result = 0
     
     while answer:
-        if count == n:
-            result=sumn%1234567
+        if count == n:				
+            result = sumn%1234567
             answer = False
             continue
-        bef=aft
-        aft=sumn
-        sumn=bef+aft
+        bef = aft					# 현재가 n 번 째가 아니라면 한 칸씩 옮긴다
+        aft = sumn
+        sumn = bef + aft			# n = (n-2 + n-1)
+        
         count+=1
         
     return result
