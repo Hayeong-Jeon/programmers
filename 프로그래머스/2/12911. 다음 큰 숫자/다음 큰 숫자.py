8e6106355a83b371f51248468c28fee74b40c986
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-14 21:55:25 +0900

    Update 다음 큰 숫자.py

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -3,9 +3,9 @@ def solution(n):
     nextn = n+1
     while answer:
         
-        if bin(n).count('1') == bin(nextn).count('1'):
+        if bin(n).count('1') == bin(nextn).count('1'): # 이진수 변환 후 '1'의 갯수 같은지
             answer = False
             continue
     
-        nextn+=1
+        nextn+=1       # 아니라면 다음 수 증가
     return nextn
