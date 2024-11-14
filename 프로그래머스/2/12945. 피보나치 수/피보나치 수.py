74080ef05445bb46c5318e53cd71042574f58702
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-14 23:54:57 +0900

    [level 2] Title: 피보나치 수, Time: 158.59 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,21 @@
+def solution(n):
+    answer = True
+    count=2
+    bef = 0
+    aft = 1
+    sumn=bef+aft
+    
+    result=0
+    while answer:
+        if count == n:
+            result=sumn%1234567
+            answer = False
+            continue
+        bef=aft
+        aft=sumn
+        sumn=bef+aft
+        count+=1
+        
+        
+        
+    return result
