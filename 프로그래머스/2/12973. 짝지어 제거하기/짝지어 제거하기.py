32775061f8aca976b040fd3fafe8723317a4616f
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-16 01:24:06 +0900

    Update 짝지어 제거하기.py

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -4,14 +4,13 @@ def solution(s):
     lists = list(s)
     stack = []
     
-
     for i in lists:
-        if len(stack) != 0 and stack[-1] == i:
-            stack.pop()
+        if len(stack) != 0 and stack[-1] == i:   # 스택이 비어있지 않으면서
+            stack.pop()							 # 마지막 값이 다음 올 문자와 같다면 pop
         else:
-            stack.append(i)
+            stack.append(i)						 # 다르다면 다음 올 문자를 push
             
-    if len(stack) ==0:
+    if len(stack) ==0:							 # 스택이 비어있다면 모두 제거됐으므로 1
         answer = 1
     else:
         answer = 0
