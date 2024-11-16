194f894141fbae416aaeb1f0c6cabb2b5c52bf6d
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-16 17:57:17 +0900

    Update 카펫.py

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,16 +1,15 @@
 def solution(brown, yellow):
-    answer = []
     total = brown + yellow
     
-    #(x-2)*(y-2) = yellow
-    #x * y = yellow + brown
-    #yellow + brown - 2x - 2y +4 = yellow
+    # 노란색 격자는 갈색 x, y보다 2가 적다는 성질을 이용해서 식을 만들 수 있다.
+    # (x-2)*(y-2) = yellow 를 전개해서
+    # x * y = yellow + brown 를 대입하면
+    # yellow + brown - 2x - 2y +4 = yellow 라는 식이 나오는 데, 정리하면
+    # x+y = brown//2 +2 이 식을 사용
     
-    #x+y = brown//2 +2
-    
-    for i in range(3,total+1):
-        if total%i ==0:
+    for i in range(3,total+1):			# x값은 항상 3이상
+        if total%i ==0:     			
             y=i
             x=total//i
-            if (x-2)*(y-2) == yellow:
+            if x+y == (brown//2 +2):	# 이 식을 만족해야만 하나의 x, y가 도출됨
                 return[x, y]
