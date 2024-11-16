ca7a9acbf3d7a1bbe29a531a75bf5405029f814b
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-16 18:00:15 +0900

    Update 최댓값과 최솟값.py

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -4,7 +4,7 @@ def solution(s):
 
     answer.append(str(min(num)))   #answer에 num배열 중 가장 큰 값, 작은 값을 문자열로 저장
     answer.append(str(max(num)))
-    
+
     string =''
     for i in answer:
         string += i + ' '    #string에는 최종적으로 "작은값 큰값 "으로 저장 되기때문에
