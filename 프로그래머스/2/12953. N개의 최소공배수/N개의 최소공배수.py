fd8d554a893fd942590688e2c85ea55490a6ab95
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-18 01:12:53 +0900

    Update N개의 최소공배수.py

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,17 +1,17 @@
 def solution(arr):
     answer = True
-    
     maxn = max(arr)
-    
-    arr.sort()
+    arr.sort()						# maxn의 값을 맨 뒤로 보내기 위함
     i = 1
+    
     while answer:
         gop = maxn * i
+        
         for j in arr:
-            if j == maxn:
-                answer = False
+            if j == maxn:		   # maxn에 도달했을 때, 모든 arr의 값이 gop의 약수이며
+                answer = False	   # 해당 gop이 최소공배수 일 것이므로 반복문 중지
                 break
-            elif gop%j==0:
+            elif gop % j == 0:
                 continue
             
             else:
