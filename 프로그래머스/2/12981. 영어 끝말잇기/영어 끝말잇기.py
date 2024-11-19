3953400629ccdbf26de2530bbc6f52bc7a3c8ed8
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-19 21:54:01 +0900

    Update 영어 끝말잇기.py

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,26 +1,26 @@
 def solution(n, words):
     lenw = len(words)
-    turn=1
-    word = set()
-    a =1
-    b =1
+    turn = 1
+    word = set()				# 다음 단어의 중복을 찾기위해 집합함수 사용
+    a = 1						# a번 째 사람
+    b = 1						# b번 째 턴
 
     for s in range(lenw -1):
-        word.add(words[s])
-        print(a,b)
+        word.add(words[s])		
         
-        if a==n:
-            a=1
-            b+=1
+        if a == n:
+            a = 1
+            b+= 1
         else:
             a+=1
-        
+            
+        # 다음 단어가 집합함수에 존재한다거나, 끝말잇기가 되지 않는다면 반복문 중지
         if words[s+1] in word or words[s][-1] != words[s+1][0]:
-            turn+=s+1
+            turn+= s+1		# 다음 턴에 턴이 끝남	
             break
 
 
-    if turn == 1:
+    if turn == 1:			# if 문에 걸리지 않았다면 turn은 변함없음
         return [0,0]
     
 
