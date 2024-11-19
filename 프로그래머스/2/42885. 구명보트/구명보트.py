c1b0395f2054b22db8b0cb303f5abe6d46c93474
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-19 23:48:30 +0900

    Update 구명보트.py

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,19 +1,23 @@
 from collections import deque
 def solution(people, limit):
     answer = 0
-    people.sort()
-    queue = deque(people)
-    while queue:
-        if len(queue) == 1:
+    people.sort()				# 올림차순 정렬
+    queue = deque(people)		# people배열 큐 삽입
+    
+    while queue:				
+        if len(queue) == 1:		# 만약 태울사람이 한명만 남는다면 구명보트 사용
+            queue.pop()			# 큐 오른쪽 값 제거
+            answer+=1
+        
+        # 작은 값 + 큰 값 <= 무게제한 일때, 두명 다 태움(큐 양쪽 값 제거)
+        elif queue[0] + queue[-1] <= limit:	
+            queue.popleft()		
             queue.pop()
             answer+=1
-            break
-        
-        if queue[0] + queue[-1] <= limit:
-            queue.popleft()
-        
-        queue.pop()
-        answer+=1
+            
+       	else:					# 무게제한 넘어갈 시 무거운사람 태움(큐 오른쪽 값 제거)
+        	queue.pop()
+        	answer+=1
         
         
         
