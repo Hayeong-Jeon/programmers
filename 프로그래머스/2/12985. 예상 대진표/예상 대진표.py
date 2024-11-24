a3254cb5b99a76e3924b3215bfbf7bbba5d1c279
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-24 16:41:22 +0900

    Update 예상 대진표.py

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,16 +1,24 @@
 def solution(n,a,b):
     answer = 0
-    
-    if a > b:
+    c=0
+    maxn = bin(n).count('0') - 1		 # 최대 라운드 ex) n = 4 = 100 이므로 2라운드
+										 # bin() 해주면 0b100로 변환되기 때문에 -1    
+    if a > b:							 # a가 b보다 커질 수 있음을 방지
         a, b = b, a
-
-    while 1:  # 참가자 수가 1보다 클 때까지 반복
-        answer += 1  # 라운드 수 증가
-
-        # A와 B가 같은 매치에 있는지 확인
-        if (a + 1) // 2 == (b + 1) // 2:
-            return answer
-    
-        # 다음 라운드로 진행
-        a = (a + 1) // 2  # A의 번호 업데이트
-        b = (b + 1) // 2  # B의 번호 업데이트
+        
+    n = n//2                             # 중간을 기준으로 판별
+    c = n
+    for i in range(maxn, 0, -1):
+        if i == 1:
+            return 1
+        
+        if (a > n and b > n):			 # 둘 다 n보다 크면 중간으로 나눈뒤 라운드 -1
+            c=c//2
+            n += c
+            continue
+            
+        elif (a <=n and b<=n):			 # 적으면 반으로 나눔
+            c=c//2
+            n -= c
+            continue
+        return i
