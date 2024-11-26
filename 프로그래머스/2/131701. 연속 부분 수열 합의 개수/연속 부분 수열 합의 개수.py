b47d57402f1eff8eb13be483a1d752e20ca95db3
hayeong jeon <127087976+Hayeong-Jeon@users.noreply.github.com>
2024-11-26 14:31:45 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 332.77 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(elements):
+    queue1 = deque(elements)
+    queue2 = deque(elements)
+    sumq = 0
+    answer = 0
+    result = set(elements)
+    lene = len(elements)
+    
+    for i in range(lene):
+        answer = queue1.popleft()
+        for j in range(i+1, i+lene):
+            answer += elements[j%lene]
+            result.add(answer)
+            
+        
+    
+    return len(result)
